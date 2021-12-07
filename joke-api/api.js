a2fa3f60a4e05c9bf8367ad50ad566c4c86b1f75
Bars <90409152+barsboss2005@users.noreply.github.com>
2021-12-07 13:46:50 +0600

    Add files via upload

---
+++ joke-api/api.js
@@ -0,0 +1,21 @@
+const joekEl = document.getElementById("joke");
+const get_joke = document.getElementById("get_joke");
+
+get_joke.addEventListener("click", generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+  //call the icons api
+  const jokeRes = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const joke = await jokeRes.json();
+
+  console.log(joke);
+  //set
+  joekEl.innerHTML = joke.joke;
+}
